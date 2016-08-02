34543f994278c09191bf13df3dceb10012cdf07e
Seth Frey <seth.frey@dartmouth.edu>
2016-08-02 13:54:00 -0400

    update dates

--- local_settings.py
+++ local_settings.py
@@ -2,6 +2,6 @@ pathLocal = '/Users/sfrey/projecto/research_projects/minecraft/redditcommunity/'
 #pathDataOut =  local_path+ 'data/'
 pathDataOut =  'data/'
 pathData =  'data/'
-pathDataInPlayers =  '/Users/sfrey/projecto_staid/minecraft/20160504/'
+pathDataInPlayers =  '/Users/sfrey/projecto_staid/minecraft/20160712/'
 pathDataInPlugins = '/Users/sfrey/projecto/research_projects/minecraft/server_surveyor/archive/'
 pathDataInMCSOrg = "/mcsdotorg_server_surveyor/archive/mcorgservers_omni_step1.txt"
