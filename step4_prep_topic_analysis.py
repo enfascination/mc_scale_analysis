834818dd852424e20541db47458d70e75549a3ff
Seth Frey <seth.frey@dartmouth.edu>
2016-08-02 13:56:23 -0400

    little refacotring and improving robustness

--- step4_prep_topic_analysis.py
+++ step4_prep_topic_analysis.py
@@ -1,15 +1,11 @@
+# -*- coding: utf-8 -*-
 from local_settings import pathData, pathDataInPlugins, pathDataInMCSOrg
 from libmcscrape import standardize_address, extract_features_from_mcjson_logs
 import csv
 import json
 import subprocess
+from pprint import pprint
 
-### now convert all those files to an r-friendly format
-print("produce server/week/plugin rows from omni logs")
-### pull the reddit scrapes together into a csn file for R to work with
-extract_features_from_mcjson_logs(pathData+"step3_scraped_reddit_posts.json", pathData+'step4_reddit_mcservers.csv')
-### pull the reddit scrapes together into a csn file for R to work with
-extract_features_from_mcjson_logs(pathData+"step3_scraped_omnimc_posts.json", pathData+'step4_omnimc_mcservers.csv')
 
 def prep_topic_analysis(pathDataIn, pathDataOut, data_source_tag, onlyUnique=True):
     visits = set()
@@ -23,14 +19,18 @@ def prep_topic_analysis(pathDataIn, pathDataOut, data_source_tag, onlyUnique=Tru
                     visit = standardize_address(mc.get('mc_addr', ''))
                     if not onlyUnique or not visit in visits:
                         visits.add(visit)
-                        mcdata_csv.writerow([
-                            mc.get('post_uid', visit)
-                            , visit
-                            , data_source_tag
-                            , (mc['title']+' '+mc['selftext']).replace('\n', ' ').replace('"', "'").replace('[[', ' ').replace(']]', ' ').replace('**', ' ').replace('[', ' ').replace(']', ' ').replace('*', ' ').replace('.', ' ').replace('/', ' ').replace('', '').replace('!', " ").replace('(', " ").replace(')', " ").replace(':', ' ')]) #, "srv_repsample" ))
+                        try:
+                            mcdata_csv.writerow([
+                                mc.get('post_uid', visit)
+                                , visit
+                                , data_source_tag
+                                , (mc['title']+' '+mc['selftext']).encode("UTF-8").replace('\n', ' ').replace('"', "'").replace('[[', ' ').replace(']]', ' ').replace('**', ' ').replace('[', ' ').replace(']', ' ').replace('*', ' ').replace('.', ' ').replace('/', ' ').replace('', '').replace('!', " ").replace('(', " ").replace(')', " ").replace(':', ' ')]) #, "srv_repsample" ))
+                        except UnicodeEncodeError:
+                            pprint(mc)
+                            raise
     print(len(visits))
 
 ### prep reddit data
-prep_topic_analysis(pathData+"step3_scraped_reddit_posts_all"+".json", pathData+"step4_reddit_texts.csv", "reddit")
+prep_topic_analysis(pathData+"step32_scraped_reddit_posts_all"+".json", pathData+"step4_reddit_texts.csv", "reddit")
 ### prep mcs_org data
-prep_topic_analysis(pathData+"step3_scraped_omnimc_posts"+".json", pathData+"step4_mcs_org_texts.csv", "mcs_org")
+prep_topic_analysis(pathData+"step32_scraped_omnimc_posts"+".json", pathData+"step4_mcs_org_texts.csv", "mcs_org")
