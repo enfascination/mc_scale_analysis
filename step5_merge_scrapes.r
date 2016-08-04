e231dea2f4808ef4cda27c0fa6036a90ad184026
Seth Frey <seth.frey@dartmouth.edu>
2016-08-04 14:26:20 -0400

    part of above large refactor

--- step5_merge_scrapes.r
+++ step5_merge_scrapes.r
@@ -22,10 +22,10 @@ splugins <- merge( splugins, splugins[feat_source=='plugin',list(plugin_count=.N
 splugins <- merge( splugins, splugins[feat_source=='keyword',list(keyword_count=.N),by=c("srv_addr")], by="srv_addr", all.x=T, all.y=F)
 splugins <- merge( splugins, splugins[feat_source=='tag',list(tag_count=.N),by=c("srv_addr")], by="srv_addr", all.x=T, all.y=F)
 splugins <- merge( splugins, splugins[feat_source=='sign',list(sign_count=.N),by=c("srv_addr")], by="srv_addr", all.x=T, all.y=F)
+splugins <- merge( splugins, splugins[feat_source=='property',list(property_count=.N),by=c("srv_addr")], by="srv_addr", all.x=T, all.y=F)
 splugins <- splugins[feat_source!='sign']
 setkey(splugins, dataset_source, post_uid, date_post, srv_addr)
-
-pluginstats <- as.data.table(read.csv(paste0(pathData, 'step45_curse_plugins_metadata_full.csv')))
+splugins[,property_count:=NULL]
 
 
 #mc_forbid  <- splugins[feat %in% word_forbid[,1], unique(srv_addr)]
@@ -110,38 +110,9 @@ spings[is.na(srv_repquery) & srv_repsample,srv_repquery := TRUE ]
 spings <- merge(spings, stopics[,!"post_uid",with=F], by=c('dataset_source','srv_addr'), all.x=T, all.y=F)
 spings[, srv_reptopic:=ifelse(!is.na(T1), T, F)]
 
-#get server/week down to server.  merge with posts, performance metrics, other things (topic data?)
-### I'M COMMITTING HERE TO MY DEPENDEENT VARIABLE, so really everything from heredown should go ina  new file, since it isn't analysis agnostic
-### keep developing spings a bit further
-#setkey(spings, dataset_source, ping_uid, date_ping, srv_addr)
-### reduce data down to one dependent type
-sserv <- spings
-sserv <- sserv[(bestweek4visits==T & bestweek30visits==T) | (is.na(bestweek4visits) & bestweek30visits==T)] ### get unique server rows, with a bias for the 4 visits measure over the 30visists measure
-sserv[,':='(bestweek30visits=NULL, bestweek4visits=NULL, ncomm30visits=NULL, ncomm4visits=NULL)] ### erase these columsn to ge tthe ones from spings instead
-sserv <- spings[bestweek4visits==T,.(srv_addr, ncomm4visits)][sserv,on=c("srv_addr")]
-sserv <- sserv[!is.na(ncomm4visits)]
-sserv <- sserv[srv_max >0]
-sserv[,srv_max_log:=log10(srv_max)]
-
-#spings <- spings[dataset_source=='reddit']
-sserv[,lapply(list(srv_repstat, srv_repquery, srv_repplug, srv_repsample, srv_repsniff, srv_reptopic), sum, na.rm=T), by=dataset_source]
-
-### implictly, an important thing happening here is that null postuid means getting rid of pings that didn't match to plugin hauls
-sfeat <- splugins[,list(post_uid, feat, feat_code, feat_type, feat_source, feat_trust, plugin_count, keyword_count, tag_count, sign_count)][sserv[!is.na(post_uid)], on=c("post_uid")]  ### CAREFUL!!!  the same IPs came from a few different data sources.  argg.
-### sfeat[is.na(srv_repsample),srv_repsample:=i.srv_repsample]  ### be explicit about what gets mapped from splugins, or you'll get bad overrides
-sfeat[,lapply(list(srv_repstat, srv_repquery, srv_repsample, srv_repsniff, srv_reptopic), sum, na.rm=T), by=dataset_source]
-expect_true(sfeat[,length(unique(srv_addr))] == sfeat[,length(unique(srv_addr)), by=dataset_source][,sum(V1)])
-expect_true(sfeat[,lapply(list(srv_repstat, srv_repquery, srv_repsample, srv_repsniff, srv_reptopic), sum, na.rm=T), by=dataset_source][3,V5] == 0)
-### merge plugin properties into sfeat
-#setnames(pluginstats, "build.y", "build")
-sfeat <- merge(sfeat, pluginstats[build=='curse', list(feat, feat_source_coded = feat_source, feat_url = url, date_created, date_updated, dls_total, dls_recent, likes, cat_admintools, cat_antigrief, cat_chat, cat_devtools, cat_economy, cat_fixes, cat_fun, cat_general, cat_informational, cat_mechanics, cat_misc, cat_roleplay, cat_teleportation, cat_webadmin, cat_world, cat_worldgen)], all.x=T, all.y=F, by=c('feat'))
-sfeat[,feat_count:=.N,by="feat_code"]
-
-
 #save everyting
-saveRDS(sserv, paste0(pathData, "step5_servers.rds"))
 saveRDS(spings, paste0(pathData, "step5_serversweeks.rds"))
-saveRDS(sfeat, paste0(pathData, "step5_serversweeksplugins.rds"))
+saveRDS(splugins, paste0(pathData, "step5_serversweeksplugins.rds"))
 #sfeat <- readRDS(paste0(pathData, "step5_serversweeksplugins.rds"))
 #spings <- readRDS(paste0(pathData, "step5_serversweeks.rds"))
 #sserv <- readRDS(paste0(pathData, "step5_servers.rds"))
